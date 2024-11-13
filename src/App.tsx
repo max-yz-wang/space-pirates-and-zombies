e5c987a2d6a061d0afb46da2fdb3ee32313a83cc
Max <max.wang@outlook.com.au>
2024-11-13 18:22:41 +1100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Root from './pages/Root';
 import PageNotFound from './pages/PageNotFound';
+import Factions from './pages/Factions';
 const App: React.FC = () => {
   return (
     <HashRouter>
@@ -15,6 +16,10 @@ const App: React.FC = () => {
         <Route
           path="/homepage"
           element={<Homepage />}
+        />
+        <Route
+          path="/factions"
+          element={<Factions />}
         />
         <h1>ligma</h1>
         <Route element={<PageNotFound />} />
---
+++ src/pages/Factions.tsx
@@ -0,0 +1,14 @@
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Factions: React.FC = () => {
+  return (
+    <>
+      <div>
+        <h1>Factions</h1>
+      </div>
+    </>
+  );
+};
+
+export default Factions;
